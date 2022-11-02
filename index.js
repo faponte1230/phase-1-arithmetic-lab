bec5677a9f360a0f669e0c71e212af430ff9ae10
Fernando Aponte <faponte1230@yahoo.com>
2022-11-01 21:26:13 -0400

    commit tree 8a8a99986020df6875d3da03d15b2d55fe4dcd6d

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply
+multiply = num1 * num2
 
-// Write your code here
+let random
+random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 19
+let num4 = 5
+let mod
+mod = num3 % num4
+
+const max = Math.max(1,2,3,20)
